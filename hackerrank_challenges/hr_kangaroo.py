87891ca5288013c6025992271c04250a87bd4655
colemai <colemai@tcd.ie>
2020-05-22 11:57:44 +0100

    Relatively efficient kangaroo submitted success

--- hackerrank_challenges/hr_kangaroo.py
+++ hackerrank_challenges/hr_kangaroo.py
@@ -10,6 +10,10 @@ Output: Str "YES" or "NO"
 2. Edge Cases
 3. Optimise
 4. Clean
+
+Edge cases
+ - if v2 > v1
+ - 
 """
 
 
@@ -19,13 +23,30 @@ import pdb
 def kangaroo(x1, v1, x2, v2):
 	""""""
 	# The following is the max number of possible steps based on given constraints
+	if v2 > v1:
+		return "NO" # kang1 will never catch up
+
+	x1_even = (min(x1, 2) % max(x1,2)) % 2 == 0
+	v1_even = max(v1, 2) % min(v1,2) == 0
+	k1_always_even = x1_even and v1_even
+	k1_always_odd = not x1_even and not v1_even
+	x2_even = (min(x2, 2) % max(x2,2)) % 2 == 0
+	v2_even = max(v2, 2) % min(v2,2) == 0
+	k2_always_even = x2_even and v2_even
+	k2_always_odd = not x2_even and not v2_even
+	if (k1_always_even and k2_always_odd) or (k1_always_odd) and (k2_always_even):
+		return "NO"
+
 	steps = range(1,10000)
 	for step in steps:
 		kang1_pos = x1 + (v1 * step)
 		kang2_pos = x2 + (v2 * step)
-		if kang1_pos == kang2_pos:
+		if kang1_pos > kang2_pos:
+			return "NO"
+		elif kang1_pos == kang2_pos:
 			return "YES"
 	return "NO"
+
 
 
 if __name__ == "__main__":
