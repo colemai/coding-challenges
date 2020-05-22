aa37d82b33ef906dc1a5cd82c255faf2b2046cd1
colemai <colemai@tcd.ie>
2020-05-22 11:33:52 +0100

    Brute solution passing for base cases

--- hackerrank_challenges/hr_kangaroo.py
+++ hackerrank_challenges/hr_kangaroo.py
@@ -6,13 +6,28 @@ https://www.hackerrank.com/challenges/kangaroo/problem
 Input: Four ints
 Output: Str "YES" or "NO"
 
+1. Brute
+2. Edge Cases
+3. Optimise
+4. Clean
 """
+
+
+
+import pdb
+
 def kangaroo(x1, v1, x2, v2):
 	""""""
-	return (x1, v1, x2, v2)
+	# The following is the max number of possible steps based on given constraints
+	steps = range(1,10000)
+	for step in steps:
+		kang1_pos = x1 + (v1 * step)
+		kang2_pos = x2 + (v2 * step)
+		if kang1_pos == kang2_pos:
+			return "YES"
+	return "NO"
 
 
 if __name__ == "__main__":
-	x1, v1, x2, v2 = 0, 3, 4, 2
-	ans = kangaroo(x1, v1, x2, v2)
-	print (ans)
+	assert kangaroo(0, 3, 4, 2 ) == "YES"
+	assert kangaroo(0, 2, 5, 3) == "NO"
