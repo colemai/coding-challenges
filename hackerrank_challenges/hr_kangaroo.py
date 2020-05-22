77caf3e7d16ffca0be4a293441ee8af20bd31ef4
colemai <colemai@tcd.ie>
2020-05-22 10:56:25 +0100

    hr_kangaroo setup

---
+++ hackerrank_challenges/hr_kangaroo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Author: Ian
+Purpose: Coding challenge as per
+https://www.hackerrank.com/challenges/kangaroo/problem
+Input: Four ints
+Output: Str "YES" or "NO"
+
+"""
+def kangaroo(x1, v1, x2, v2):
+	""""""
+	return (x1, v1, x2, v2)
+
+
+if __name__ == "__main__":
+	x1, v1, x2, v2 = 0, 3, 4, 2
+	ans = kangaroo(x1, v1, x2, v2)
+	print (ans)
