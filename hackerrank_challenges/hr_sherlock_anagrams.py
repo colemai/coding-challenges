05fd4316a44ab08d22775775d9a29acb36fb5161
colemai <colemai@tcd.ie>
2020-05-24 16:01:38 +0100

    Base case running for sherlock anagrams

--- hackerrank_challenges/hr_sherlock_anagrams.py
+++ hackerrank_challenges/hr_sherlock_anagrams.py
@@ -8,20 +8,35 @@ Output: Int
 """
 
 import pdb
+import math
 
 def sherlockAndAnagrams(s):
-	pass
-
+	# For each size of sub string, collect all substrings
+	# sort each substring alpabetically
+	# count the occurence of each element
+	# for each, add count -1 to anagrams
+	anagrams = 0
+	for substring_size in range(1, len(s)):
+		substrings = []
+		for position in range(0,(len(s) - substring_size) + 1):
+			substrings.append(s[position:(position + substring_size)])
+		substrings = [''.join(sorted(x)) for x in substrings]
+		counts = dict((x,substrings.count(x)) for x in set(substrings))
+		for count in counts.values():
+			count -= 1
+			if count > 0:
+				anagrams += ((count * count) + count)/2
+	return int(anagrams)
 
 if __name__ == "__main__":
-	s = "mom"
+	s = "kkkk"
 	ans = sherlockAndAnagrams(s)
 	print(ans)
 
-	# assert sherlockAndAnagrams("mom") == 2
-	# assert sherlockAndAnagrams("abba") == 4
-	# assert sherlockAndAnagrams("abcd") == 0
-	# assert sherlockAndAnagrams("ifailuhkqq") == 3
-	# assert sherlockAndAnagrams("kkkk") == 10
-	# assert sherlockAndAnagrams("cdcd") == 5
+	assert sherlockAndAnagrams("mom") == 2
+	assert sherlockAndAnagrams("abba") == 4
+	assert sherlockAndAnagrams("abcd") == 0
+	assert sherlockAndAnagrams("ifailuhkqq") == 3
+	assert sherlockAndAnagrams("kkkk") == 10
+	assert sherlockAndAnagrams("cdcd") == 5
 	
