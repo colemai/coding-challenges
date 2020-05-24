d093caafc6864efc05c9fe1909175a7180701d4d
colemai <colemai@tcd.ie>
2020-05-24 12:54:10 +0100

    hr sherlock anagrams skeleton in place

---
+++ hackerrank_challenges/hr_sherlock_anagrams.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+Author: Ian
+Purpose: Coding challenge as per https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem
+Input: String
+Output: Int
+"""
+
+import pdb
+
+def sherlockAndAnagrams(s):
+	pass
+
+
+if __name__ == "__main__":
+	s = "mom"
+	ans = sherlockAndAnagrams(s)
+	print(ans)
+
+	# assert sherlockAndAnagrams("mom") == 2
+	# assert sherlockAndAnagrams("abba") == 4
+	# assert sherlockAndAnagrams("abcd") == 0
+	# assert sherlockAndAnagrams("ifailuhkqq") == 3
+	# assert sherlockAndAnagrams("kkkk") == 10
+	# assert sherlockAndAnagrams("cdcd") == 5
+	
