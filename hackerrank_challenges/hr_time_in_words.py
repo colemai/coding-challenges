4524db2f61d405bc4e767aeb32e46ea4f8cbff36
colemai <colemai@tcd.ie>
2020-05-24 10:11:12 +0100

    hr_time_in_words skeleton incl tests

---
+++ hackerrank_challenges/hr_time_in_words.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+Author: Ian
+Purpose: Coding challenge as per https://www.hackerrank.com/challenges/the-time-in-words/problem
+Input: String time e.g 12:15
+Output: String time in words
+"""
+
+import pdb
+
+def timeInWords(h, m):
+	""""""
+
+
+
+if __name__ == "__main__":
+
+	ans = timeInWords(7,15)
+	
+	print(ans)
+
+	# assert timeInWords(3,00) == "three o' clock"
+	# assert timeInWords(3,0) == "three o' clock"
+	# assert timeInWords(12,30) == "half past twelve"
+	# assert timeInWords(12,52) == "eight minutes to one"
