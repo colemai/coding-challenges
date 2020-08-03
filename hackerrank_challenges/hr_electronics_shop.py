610aa0243dced7c37d1cf1df0e4a272a13a33e31
colemai <colemai@tcd.ie>
2020-08-03 14:52:34 +0100

    Init hr electronics shop

---
+++ hackerrank_challenges/hr_electronics_shop.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+Author: Ian
+Purpose: Coding challenge as per https://www.hackerrank.com/challenges/electronics-shop/problem
+Input: 
+Output: 
+"""
+
+def getMoneySpent(keyboards, drives, b):
+    #
+    # Write your code here.
+    #
+    return -1
+
+
+if __name__ == "__main__":
+	b = 10
+	keyboards = [3, 1]
+	drives = [5, 2, 8]
+	
+	ans = getMoneySpent(keyboards, drives, b)
+
+	print(ans)
+	
