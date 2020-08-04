2b118faa23325e3b7ddaca104d873ae7bf86aaa9
colemai <colemai@tcd.ie>
2020-08-04 11:48:22 +0100

    Task 1 done

--- other_challenges/dalton_coleman.py
+++ other_challenges/dalton_coleman.py
@@ -16,19 +16,42 @@ Plan:
 5. Adapt or rewrite for readability, modularity, PEP8 style
 6. Adapt or rewrite for efficency
 7. Adapt or rewrite for security
+8. Sanity checks
+
+
+Task:
+1. Create n by n grid
+2. 
 """
 
 
 import pdb
 
+def print_grid(n):
+    grid = []
+
+    column_numbers = [str(x) for x in range(1, n + 1)]
+    first_line = "  " + "  ".join(column_numbers)
+    grid.append(first_line)
+
+    horizontal_unit = "+--"
+    vertical_unit = "|  "
+
+    for row in range(1, n + 1):
+        grid += [" " + (horizontal_unit * n) + "+"]
+        grid += [str(row) + (vertical_unit * n) + "|"]
+    grid += [" " + (horizontal_unit * n) + "+"]
+
+    return grid
+
 
 if __name__ == "__main__":
-	
-	ans = 
-	print(ans)
+    
+    ans = print_grid(3)
+    [print(x) for x in ans]
 
-	# assert sherlockAndAnagrams("mom") == 2
-	# assert type(options) is dict
-	# assert 'message' in options
-	# assert isinstance(options['message'], str)
-	# assert bool(options) == True
+    # assert sherlockAndAnagrams("mom") == 2
+    # assert type(options) is dict
+    # assert 'message' in options
+    # assert isinstance(options['message'], str)
+    # assert bool(options) == True
