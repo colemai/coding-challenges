05f637bd225d8f01616668df257a432da8aca956
colemai <colemai@tcd.ie>
2020-08-04 11:05:25 +0100

    init dalton_coleman challenge

---
+++ other_challenges/dalton_coleman.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""
+Author: Ian Coleman
+Purpose: Coding challenge
+Input: 
+Output: 
+Example Run: 
+
+
+Plan:
+1. Read challenge three times
+2. Plan
+3. Implement brute solution
+4. Account for edge cases
+5. Adapt or rewrite for readability, modularity, PEP8 style
+6. Adapt or rewrite for efficency
+7. Adapt or rewrite for security
+"""
+
+
+import pdb
+
+
+if __name__ == "__main__":
+	
+	ans = 
+	print(ans)
+
+	# assert sherlockAndAnagrams("mom") == 2
+	# assert type(options) is dict
+	# assert 'message' in options
+	# assert isinstance(options['message'], str)
+	# assert bool(options) == True
